f663f217acd0fc14e2f5d1d5cc45307d382345d8
csscoceanjeffery <hanhaipeng-2005@163.com>
2020-04-29 20:00:24 +0800

    week02

---
+++ week02/utf-8encoding.js
@@ -0,0 +1,31 @@
+let writeUTF = function (str) {
+    let back = [];
+    let byteSize = 0;
+    for (let i = 0; i < str.length; i++) {
+        const code = str.charCodeAt(i);
+        if (0x00 <= code && code <= 0x7f) {
+            byteSize += 1;
+            back.push(code);
+        } else if (0x80 <= code && code <= 0x7ff) {
+            byteSize += 2;
+            back.push((192 | (31 & (code >> 6))));
+            back.push((128 | (63 & code)))
+        } else if ((0x800 <= code && code <= 0xd7ff)
+            || (0xe000 <= code && code <= 0xffff)) {
+            byteSize += 3;
+            back.push((224 | (15 & (code >> 12))));
+            back.push((128 | (63 & (code >> 6))));
+            back.push((128 | (63 & code)))
+        }
+    }
+    for (let i = 0; i < back.length; i++) {
+        back[i] &= 0xff;
+    }
+    if (byteSize <= 0xff) {
+        return [0, byteSize].concat(back);
+    } else {
+        return [byteSize >> 8, byteSize & 0xff].concat(back);
+    }
+};
+
+console.log(writeUTF("中国人民"));
